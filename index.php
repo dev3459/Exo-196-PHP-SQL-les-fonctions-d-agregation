d92c4f2fe3eda36b36386e638405c3947cfee8cb
dev3459 <test45@outlook.fr>
2021-03-02 10:37:28 +0100

    Exo 196 Fini

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Exo 196</title>
 </head>
 <body>
     <?php
@@ -24,9 +24,49 @@
      */
 
     // TODO Votre code ici, commencez par require un des objet de connexion que nous avons fait ensemble.
+    require 'db.php';
+    $db = DB::getInstance();
+    $request = $db->prepare("SELECT MIN(age) as 'minimum' FROM exo_196_users.user");
+    $request->execute();
 
+    foreach ($request->fetchAll() as $user) { ?>
+        <p>L'âge minimum est <?= $user['minimum'] ?></p>
+    <?php }
 
-    ?>
+    $request = $db->prepare("SELECT MAX(age) as 'maximum' FROM exo_196_users.user");
+    $request->execute();
+
+    foreach ($request->fetchAll() as $user) { ?>
+    <p>L'âge maximum est <?= $user['maximum'] ?></p>
+    <?php }
+
+    $request = $db->prepare("SELECT COUNT(id) as 'user' FROM exo_196_users.user");
+    $request->execute();
+
+    foreach ($request->fetchAll() as $user) { ?>
+    <p>Il y a <?= $user['user'] ?> utilisateurs pour le moment</p>
+    <?php }
+
+    $request = $db->prepare("SELECT COUNT(id) as 'numero' FROM exo_196_users.user WHERE numero >= 5");
+    $request->execute();
+
+    foreach ($request->fetchAll() as $user) { ?>
+        <p>Il y a <?= $user['numero'] ?> utilisateur qui ont un numéro de rue plus grand ou égal à 5</p>
+    <?php }
+
+    $request = $db->prepare("SELECT AVG(age) as 'moyenne' FROM exo_196_users.user");
+    $request->execute();
+
+    foreach ($request->fetchAll() as $user) { ?>
+        <p>La moyenne des âges des utilisateurs est de :  <?= $user['moyenne'] ?> ans</p>
+    <?php }
+
+    $request = $db->prepare("SELECT SUM(numero) as 'somme' FROM exo_196_users.user");
+    $request->execute();
+
+    foreach ($request->fetchAll() as $user) { ?>
+        <p>La somme de tout les numéro de maison des utilisateurs est de :  <?= $user['somme'] ?></p>
+    <?php } ?>
 </body>
 </html>
 
